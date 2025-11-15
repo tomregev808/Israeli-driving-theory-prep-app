07a0a0046356f4bda2ecce550bb97c156d22ae68
Tom <tomregev808@gmail.com>
2025-11-15 13:46:35 +0200

    requirments.txt

--- app/frontend.py
+++ app/frontend.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, make_response
+from flask import Blueprint, render_template, request, redirect, url_for
 from app import shared
 
 
